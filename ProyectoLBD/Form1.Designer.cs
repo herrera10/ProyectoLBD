59c1c6610d696c0ca5a342074d8294d6894bb61a
herrera10 <41028500+herrera10@users.noreply.github.com>
2019-07-18 15:09:16 -0600

    Initial commit

---
+++ ProyectoLBD/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ProyectoLBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargarEmpleados = new System.Windows.Forms.Button();
+            this.dgvEmpleados = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCargarEmpleados
+            // 
+            this.btnCargarEmpleados.Location = new System.Drawing.Point(22, 53);
+            this.btnCargarEmpleados.Name = "btnCargarEmpleados";
+            this.btnCargarEmpleados.Size = new System.Drawing.Size(174, 47);
+            this.btnCargarEmpleados.TabIndex = 0;
+            this.btnCargarEmpleados.Text = "Cargar Empleados";
+            this.btnCargarEmpleados.UseVisualStyleBackColor = true;
+            this.btnCargarEmpleados.Click += new System.EventHandler(this.btnCargarEmpleados_Click);
+            // 
+            // dgvEmpleados
+            // 
+            this.dgvEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpleados.Location = new System.Drawing.Point(22, 106);
+            this.dgvEmpleados.Name = "dgvEmpleados";
+            this.dgvEmpleados.Size = new System.Drawing.Size(1163, 283);
+            this.dgvEmpleados.TabIndex = 1;
+            this.dgvEmpleados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmpleados_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 450);
+            this.Controls.Add(this.dgvEmpleados);
+            this.Controls.Add(this.btnCargarEmpleados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargarEmpleados;
+        private System.Windows.Forms.DataGridView dgvEmpleados;
+    }
+}
+
---
+++ ProyectoLBD/Form1.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+namespace ProyectoLBD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string conString = "User Id=hr;Password=hr;Data Source=localhost:1521/xe;";
+
+        private void btnCargarEmpleados_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OracleConnection con = new OracleConnection();
+                con.ConnectionString = conString;
+                con.Open();
+                
+                                string sql = "select * from employees where department_id=90";
+                                OracleCommand cmd = new OracleCommand(sql, con);
+                // dgvEmpleados.DataSource = cmd.ExecuteReader();
+                // con.Close();*/
+
+
+                using (OracleDataReader reader = cmd.ExecuteReader())
+                {
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(reader);
+                    dgvEmpleados.DataSource = dataTable;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+            
+
+
+        }
+
+        private void dgvEmpleados_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
