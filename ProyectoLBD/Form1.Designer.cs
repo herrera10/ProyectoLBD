c364b2b1a427e3734845c7d0d9c7fb4aed8ddad6
herrera10 <41028500+herrera10@users.noreply.github.com>
2019-07-19 14:43:50 -0600

    AGREGANDO UN POCO DEL CRUD EN C#
    AGREGANDO UN POCO DEL CRUD EN C#

--- ProyectoLBD/Form1.Designer.cs
+++ ProyectoLBD/Form1.Designer.cs
@@ -28,48 +28,246 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnCargarEmpleados = new System.Windows.Forms.Button();
             this.dgvEmpleados = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtPuesto = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.dTPFecha = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnResetear = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnCargarEmpleados
-            // 
-            this.btnCargarEmpleados.Location = new System.Drawing.Point(22, 53);
-            this.btnCargarEmpleados.Name = "btnCargarEmpleados";
-            this.btnCargarEmpleados.Size = new System.Drawing.Size(174, 47);
-            this.btnCargarEmpleados.TabIndex = 0;
-            this.btnCargarEmpleados.Text = "Cargar Empleados";
-            this.btnCargarEmpleados.UseVisualStyleBackColor = true;
-            this.btnCargarEmpleados.Click += new System.EventHandler(this.btnCargarEmpleados_Click);
-            // 
             // dgvEmpleados
             // 
+            this.dgvEmpleados.AllowUserToAddRows = false;
+            this.dgvEmpleados.AllowUserToDeleteRows = false;
+            this.dgvEmpleados.AllowUserToOrderColumns = true;
+            this.dgvEmpleados.AllowUserToResizeColumns = false;
+            this.dgvEmpleados.AllowUserToResizeRows = false;
+            this.dgvEmpleados.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvEmpleados.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical;
             this.dgvEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvEmpleados.Location = new System.Drawing.Point(22, 106);
+            this.dgvEmpleados.Location = new System.Drawing.Point(515, 51);
             this.dgvEmpleados.Name = "dgvEmpleados";
-            this.dgvEmpleados.Size = new System.Drawing.Size(1163, 283);
+            this.dgvEmpleados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvEmpleados.Size = new System.Drawing.Size(670, 283);
             this.dgvEmpleados.TabIndex = 1;
+            this.dgvEmpleados.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmpleados_CellClick);
             this.dgvEmpleados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmpleados_CellContentClick);
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "EMPLOYEE_ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "FIRST_NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "LAST_NAME";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "JOB_ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "SALARY";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "HIRE_DATE";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(164, 51);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(200, 20);
+            this.txtEmployeeID.TabIndex = 8;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(164, 237);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(198, 20);
+            this.txtSalary.TabIndex = 9;
+            // 
+            // txtPuesto
+            // 
+            this.txtPuesto.Location = new System.Drawing.Point(164, 188);
+            this.txtPuesto.Name = "txtPuesto";
+            this.txtPuesto.Size = new System.Drawing.Size(198, 20);
+            this.txtPuesto.TabIndex = 10;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(164, 138);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(200, 20);
+            this.txtApellido.TabIndex = 11;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(164, 95);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 20);
+            this.txtNombre.TabIndex = 12;
+            // 
+            // dTPFecha
+            // 
+            this.dTPFecha.Location = new System.Drawing.Point(164, 289);
+            this.dTPFecha.Name = "dTPFecha";
+            this.dTPFecha.Size = new System.Drawing.Size(200, 20);
+            this.dTPFecha.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(385, 335);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informacion del Empleado";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(13, 370);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 15;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.Location = new System.Drawing.Point(106, 370);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 16;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Enabled = false;
+            this.btnActualizar.Location = new System.Drawing.Point(200, 370);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 17;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnResetear
+            // 
+            this.btnResetear.Location = new System.Drawing.Point(289, 370);
+            this.btnResetear.Name = "btnResetear";
+            this.btnResetear.Size = new System.Drawing.Size(75, 23);
+            this.btnResetear.TabIndex = 18;
+            this.btnResetear.Text = "Resetear";
+            this.btnResetear.UseVisualStyleBackColor = true;
+            this.btnResetear.Click += new System.EventHandler(this.btnResetear_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1197, 450);
+            this.Controls.Add(this.btnResetear);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dTPFecha);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtPuesto);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvEmpleados);
-            this.Controls.Add(this.btnCargarEmpleados);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Proyecto";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnCargarEmpleados;
         private System.Windows.Forms.DataGridView dgvEmpleados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtPuesto;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.DateTimePicker dTPFecha;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnResetear;
     }
 }
 
--- ProyectoLBD/Form1.cs
+++ ProyectoLBD/Form1.cs
@@ -10,54 +10,181 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Oracle.ManagedDataAccess.Client;
 using Oracle.ManagedDataAccess.Types;
+using System.Configuration;
+using System.Windows;
+
 
 namespace ProyectoLBD
 {
     public partial class Form1 : Form
     {
+        string conString = "User Id=hr;Password=hr;Data Source=localhost:1521/xe;";
+
+
         public Form1()
         {
             InitializeComponent();
+
         }
 
-        string conString = "User Id=hr;Password=hr;Data Source=localhost:1521/xe;";
-
-        private void btnCargarEmpleados_Click(object sender, EventArgs e)
+        private void updateDataGridEmpleados()
         {
-            try
+            OracleConnection con = new OracleConnection();
+            con.ConnectionString = conString;
+            con.Open();
+
+            string sql = "select  employee_id,first_name,last_name,job_id,salary,hire_date from employees ";
+            OracleCommand cmd = new OracleCommand(sql, con);
+            using (OracleDataReader reader = cmd.ExecuteReader())
             {
-                OracleConnection con = new OracleConnection();
-                con.ConnectionString = conString;
-                con.Open();
-                
-                                string sql = "select * from employees where department_id=90";
-                                OracleCommand cmd = new OracleCommand(sql, con);
-                // dgvEmpleados.DataSource = cmd.ExecuteReader();
-                // con.Close();*/
-
-
-                using (OracleDataReader reader = cmd.ExecuteReader())
-                {
-                    DataTable dataTable = new DataTable();
-                    dataTable.Load(reader);
-                    dgvEmpleados.DataSource = dataTable;
-                }
-
+                DataTable dataTable = new DataTable();
+                dataTable.Load(reader);
+                dgvEmpleados.DataSource = dataTable;
             }
-            catch (Exception ex)
-            {
-
-                MessageBox.Show(ex.Message);
-            }
-
-            
-
-
         }
 
         private void dgvEmpleados_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+          
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            updateDataGridEmpleados();
+        }
+
+
+
+        private void AUD(String sql_stmt, int state)
+        {
+            String msg = "";
+            OracleConnection con = new OracleConnection();
+            con.ConnectionString = conString;
+            con.Open();
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = sql_stmt;
+            cmd.CommandType = CommandType.Text;
+
+            switch (state)
+            {
+                case 0:
+                    msg = "Fila insertada exitosamente";
+                    cmd.Parameters.Add("EMPLOYEE_ID", OracleDbType.Int32, 6).Value = txtEmployeeID.Text;
+                    cmd.Parameters.Add("FIRST_NAME", OracleDbType.Varchar2, 25).Value = txtNombre.Text;
+                    cmd.Parameters.Add("LAST_NAME", OracleDbType.Varchar2, 25).Value = txtApellido.Text;
+                    cmd.Parameters.Add("JOB_ID", OracleDbType.Varchar2, 25).Value = txtPuesto.Text;
+                    cmd.Parameters.Add("SALARY", OracleDbType.Int32, 25).Value = txtSalary.Text;
+                    cmd.Parameters.Add("HIRE_DATE", OracleDbType.Date, 7).Value = dTPFecha.Value;
+                    break;
+
+                case 1:
+                    msg = "Fila actualizada exitosamente";
+                    
+                    cmd.Parameters.Add("FIRST_NAME", OracleDbType.Varchar2, 25).Value = txtNombre.Text;
+                    cmd.Parameters.Add("LAST_NAME", OracleDbType.Varchar2, 25).Value = txtApellido.Text;
+                    cmd.Parameters.Add("SALARY", OracleDbType.Int32, 25).Value = txtSalary.Text;
+                     //cmd.Parameters.Add("HIRE_DATE", OracleDbType.Date, 7).Value = dTPFecha.Value;
+                    cmd.Parameters.Add("EMPLOYEE_ID", OracleDbType.Int32, 6).Value = txtEmployeeID.Text;
+                   
+
+
+                    break;
+
+                case 2:
+                    msg = "Fila borrada exitosamente";
+
+                    cmd.Parameters.Add("EMPLOYEE_ID", OracleDbType.Int32, 6).Value = txtEmployeeID.Text;
+                    break;
+            }
+            try
+            {
+                int n = cmd.ExecuteNonQuery();
+                if (n > 0)
+                {
+                    MessageBox.Show(msg);
+                    this.updateDataGridEmpleados();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("" + ex);
+            //}
+            con.Close();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string sql = "INSERT INTO EMPLOYEES(EMPLOYEE_ID,FIRST_NAME,LAST_NAME,JOB_ID,SALARY,HIRE_DATE)" +
+                "VALUES(:EMPLOYEE_ID,:FIRST_NAME,:LAST_NAME,:JOB_ID,:HIRE_DATE)";
+            this.AUD(sql, 0);
+            btnAgregar.Enabled = false;
+            btnActualizar.Enabled = true;
+            btnEliminar.Enabled = true;
+        }
+
+       
+        private void dgvEmpleados_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow row = this.dgvEmpleados.Rows[e.RowIndex];
+                    txtEmployeeID.Text = row.Cells["EMPLOYEE_ID"].Value.ToString();
+                    txtNombre.Text = row.Cells["FIRST_NAME"].Value.ToString();
+                    txtApellido.Text = row.Cells["LAST_NAME"].Value.ToString();
+                    txtPuesto.Text = row.Cells["JOB_ID"].Value.ToString();
+                    txtSalary.Text = row.Cells["SALARY"].Value.ToString();
+                    dTPFecha.Value = DateTime.Parse(row.Cells["HIRE_DATE"].Value.ToString());
+
+                    btnAgregar.Enabled = false;
+                    btnActualizar.Enabled = true;
+                    btnEliminar.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("" + ex);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            String sql = "Update HR.EMPLOYEES SET FIRST_NAME = :FIRST_NAME," +
+                "LAST_NAME = :LAST_NAME,SALARY=:SALARY" +
+                "WHERE EMPLOYEE_ID = :EMPLOYEE_ID";
+            this.AUD(sql, 1);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string sql = "DELETE FROM EMPLOYEES "+
+                "WHERE EMPLOYEE_ID = :EMPLOYEE_ID";
+            this.AUD(sql, 2);
+            this.resetALL();
+        }
+
+        private void resetALL()
+        {
+            txtEmployeeID.Text = "";
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtPuesto.Text = "";
+            txtSalary.Text = "";
+           // dTPFecha = null;
+
+            btnAgregar.Enabled = true;
+            btnActualizar.Enabled = false;
+            btnEliminar.Enabled = false;
+        }
+
+        private void btnResetear_Click(object sender, EventArgs e)
+        {
+            this.resetALL();
         }
     }
 }
